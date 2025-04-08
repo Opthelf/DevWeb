1506da51255bfdb7e110bdd0ee3fde6d54f989ee
simon <simon2002huang@gmail.com>
2025-04-08 15:12:42 +0200

    LoginSuccessHandler OK

---
+++ src/Security/LoginSuccessHandler.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Security;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationSuccessHandlerInterface;
+use Symfony\Component\Routing\RouterInterface;
+
+class LoginSuccessHandler implements AuthenticationSuccessHandlerInterface
+{
+    private RouterInterface $router;
+    private RequestStack $requestStack;
+
+    public function __construct(RouterInterface $router, RequestStack $requestStack)
+    {
+        $this->router = $router;
+        $this->requestStack = $requestStack;
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token): Response
+    {
+        // Récupérer l'utilisateur connecté
+        $user = $token->getUser();
+
+        // Récupérer la session via RequestStack
+        $session = $this->requestStack->getSession();
+
+        // Ajouter l'ID et le username à la session
+        $session->set('id', $user->getId());
+        $session->set('username', $user->getUsername());
+
+        // Rediriger vers la page d'accueil ou une autre page
+        return new Response('', Response::HTTP_OK);
+    }
+}
