e6a39bd7a373b9f8f482ef3fbb609aea874e6f24
simon <simon2002huang@gmail.com>
2025-04-09 11:40:08 +0200

    rediriger vers la page d'accueil à la fin d'une inscription

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -32,8 +32,8 @@ class RegistrationController extends AbstractController
             $entityManager->flush();
 
             // do anything else you need here, like send an email
-            return $security->login($user, 'form_login', 'main');
-            return $this->redirectToRoute('home');
+            //return $security->login($user, 'form_login', 'main');
+            return $this->redirectToRoute('app_homepage');
         }
 
         return $this->render('registration/register.html.twig', [
