8e5dd2911602245d5a8e87ae56732789ef3f2d2b
simon <simon2002huang@gmail.com>
2025-04-09 11:31:13 +0200

    bug mdp réglé

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -2,39 +2,25 @@
 
 namespace App\Controller\Admin;
 
-
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
-
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Doctrine\ORM\EntityManagerInterface;
-use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
-use EasyCorp\Bundle\EasyAdminBundle\Filter\BooleanFilter;
-use EasyCorp\Bundle\EasyAdminBundle\Dto\SearchDto;
-use EasyCorp\Bundle\EasyAdminBundle\Dto\EntityDto;
-use EasyCorp\Bundle\EasyAdminBundle\Collection\FieldCollection;
-use EasyCorp\Bundle\EasyAdminBundle\Collection\FilterCollection;
-use App\Service\UserActionLogger;
+
 
 
 class UserCrudController extends AbstractCrudController
 {
     private UserPasswordHasherInterface $passwordHasher;
-    private UserActionLogger $actionLogger;
 
-
-    public function __construct(UserPasswordHasherInterface $passwordHasher, UserActionLogger $actionLogger)
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
     {
         $this->passwordHasher = $passwordHasher;
-        $this->actionLogger = $actionLogger;
-        // Inject the password hasher service
     }
 
     public static function getEntityFqcn(): string
@@ -57,21 +43,7 @@ class UserCrudController extends AbstractCrudController
             ->allowMultipleChoices() // Permet de choisir plusieurs rôles
             ->setHelp('Choisissez un ou plusieurs rôles'),
             TextField::new('password')->onlyOnForms(), // Masque le champ mot de passe dans la liste
-            BooleanField::new('isApproved', 'Approved'),
-            NumberField::new('points', 'Points') // Ajout du champ points
-            ->setHelp('Nombre de points attribués à l\'utilisateur'),
         ];
-    }
-
-    public function configureFilters(Filters $filters): Filters
-    {
-        return $filters
-            ->add(BooleanFilter::new('isApproved', 'Approved'));
-    }
-
-    public function createIndexQueryBuilder(SearchDto $searchDto, EntityDto $entityDto, FieldCollection $fields, FilterCollection $filters): \Doctrine\ORM\QueryBuilder
-    {
-        return parent::createIndexQueryBuilder($searchDto, $entityDto, $fields, $filters);
     }
 
     public function persistEntity(EntityManagerInterface $entityManager, $entityInstance): void
@@ -93,6 +65,5 @@ class UserCrudController extends AbstractCrudController
 
         parent::updateEntity($entityManager, $entityInstance);
     }
-    
     
 }
