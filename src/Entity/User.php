b57e17e185734a961d09b957b524a89f3523bd8c
simon <simon2002huang@gmail.com>
2025-04-10 15:04:28 +0200

    ajout nom prénom user

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -59,6 +59,12 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column(type: Types::DATE_MUTABLE, nullable: true)]
     private ?\DateTimeInterface $birthd = null;
 
+    #[ORM\Column(length: 255)]
+    private ?string $nom = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $prenom = null;
+
 
     public function getId(): ?int
     {
@@ -227,6 +233,30 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function setBirthd(?\DateTimeInterface $birthd): static
     {
         $this->birthd = $birthd;
+
+        return $this;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(string $prenom): static
+    {
+        $this->prenom = $prenom;
 
         return $this;
     }
